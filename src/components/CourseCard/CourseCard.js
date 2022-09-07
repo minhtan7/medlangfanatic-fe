a7a36b5b72b3f94e24240d1fbb8a4907c5a0ca82
Tan Vo <tan.vopm@gmail.com>
2022-09-07 17:24:26 +0100

    add google data analytic

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -2,12 +2,16 @@ import { faCheckSquare, faFileText, faIdCard, faNoteSticky, faPlayCircle, faSdCa
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Button, Card, ListGroup, Placeholder } from 'react-bootstrap'
+import useScript from '../../hook/useScript'
 import { formatTime } from '../../utility/formatTime'
 import { Timer } from '../Timer/Timer'
 import "./style.css"
 
 export const CourseCard = ({ course }) => {
     const { hours, minutes, seconds } = formatTime(course?.duration || 0)
+    useScript("https://tools.luckyorange.com/core/lo.js?site-id=a0c6a81c")
+    useScript("https://www.googletagmanager.com/gtag/js?id=G-NJYDR7ML63")
+
     return course ?
         (<Card className='course-card d-none d-sm-none d-md-block'>
             < Card.Img variant="top" src="/images/thumbnail.svg" style={{ borderRadius: 0 }
